709d39d59d964b9004890040bdabb3c8ce870ced
NgShow <87817554+Thecosy@users.noreply.github.com>
2022-04-26 21:16:39 +0800

    根据classid查询对应的资源分类名称

--- IceWk-ment/src/main/java/com/ttice/icewkment/controller/WebResourceClassController.java
+++ IceWk-ment/src/main/java/com/ttice/icewkment/controller/WebResourceClassController.java
@@ -1,13 +1,13 @@
 package com.ttice.icewkment.controller;
 
 
-import com.ttice.icewkment.entity.ArticleClass;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ttice.icewkment.entity.ResourceClass;
 import com.ttice.icewkment.mapper.ResourceClassMapper;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +34,17 @@ public class WebResourceClassController {
     public List<ResourceClass> getResourceClasslist(
     ){
         return resourceClassMapper.selectList(null);
+    }
+
+    @ApiOperation(value = "根据classid查询对应的资源分类名称")
+    @GetMapping("/getResourceClassNameByid/{classId}")
+    public String getResourceClassNameByid(
+            @PathVariable Integer classId
+    ){
+        QueryWrapper<ResourceClass> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id",classId);
+        ResourceClass resourceClass = resourceClassMapper.selectOne(queryWrapper);
+        return resourceClass.getName();
     }
 }
 
