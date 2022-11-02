75d5443c86fdcb1d0d85126c87df55ea24ae46df
NgShow <87817554+Thecosy@users.noreply.github.com>
2022-11-02 18:21:39 +0800

    no

--- IceWK-uniApp/unpackage/dist/build/mp-weixin/pages/appsort/appsort-n-waterfall-view-left.js
+++
@@ -1,6 +0,0 @@
-
-wx.createComponent({
-    generic:true,
-    props: {lefts:{type:null}},
-    render: function(){}
-})
--- IceWK-uniApp/unpackage/dist/build/mp-weixin/pages/appsort/appsort-n-waterfall-view-right.js
+++
@@ -1,6 +0,0 @@
-
-wx.createComponent({
-    generic:true,
-    props: {rights:{type:null}},
-    render: function(){}
-})
